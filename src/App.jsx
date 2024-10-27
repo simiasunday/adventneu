a0fd6a99e53fb816f8dd451daee95fe62a10222f
simisunday <simia_karegar@live.de>
2024-10-27 21:50:01 +0100

    adventneu hinzugefügt

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header } from './components/Header'
+import { Calendar } from "./components/Calendar";
+import './App.css'
+
+function App() {
+   return (
+    <div>
+      <Header/>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
